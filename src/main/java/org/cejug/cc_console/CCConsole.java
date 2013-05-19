39e9526893c6cd6b792f1c9da8f9e77675842f7c
heliofrota <heliofrota@gmail.com>
2013-05-19 12:05:19 -0300

    comentários adicionados

--- src/main/java/org/cejug/cc_console/CCConsole.java
+++ src/main/java/org/cejug/cc_console/CCConsole.java
@@ -1,34 +1,28 @@
 package org.cejug.cc_console;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import org.cejug.cc_console.jdbc.CCConsoleJDBC;
 
+/**
+ * Classe principal para inciar o sistema.
+ * @author helio frota https://www.heliofrota.com
+ *
+ */
 public class CCConsole {
 
-    public static void main(String[] args) {
+    /**
+     * Método main.
+     * @param String... args
+     */
+    public static void main(String... args) {
 
         System.out.println("=======");
-        System.out.println(getConnection());
-
-    }
-
-    public static Connection getConnection() {
-        Connection conn = null;
-        try {
-            // Carrega o driver do derby embarcado.
-            Class.forName("org.apache.derby.jdbc.EmbeddedDriver").newInstance();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try {
-            // Conecta no banco sem usuario e senha.
-            conn = DriverManager.getConnection("jdbc:derby:cc_console_db;create=true");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
         
-        return conn;
+        CCConsoleJDBC ccConsoleJDBC = new CCConsoleJDBC(); 
+        
+        System.out.println(ccConsoleJDBC.getConnection());
+
     }
+
+   
 
 }
--- src/main/java/org/cejug/cc_console/jdbc/CCConsoleJDBC.java
+++ src/main/java/org/cejug/cc_console/jdbc/CCConsoleJDBC.java
@@ -4,27 +4,44 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Classe que irá lidar com o acesso e manipulação dos dados
+ * no banco de dados derby embarcado usando JDBC. 
+ * 
+ * @author helio frota https://www.heliofrota.com
+ * 
+ */
 public class CCConsoleJDBC {
 
-    
-    public static void main(String[] args) {
-        
+    /**
+     * Obtém uma conexão com o banco de dados derby embarcado.
+     * 
+     * @return Connection
+     */
+    public Connection getConnection() {
+        // Referência para java.sql.Connection.
+        Connection conn = null;
+        // Tenta carregar o driver do derby embarcado, caso contrário
+        // imprime uma exceção.
         try {
-            System.out.println(getConnection().getCatalog());
+            // Carrega o driver do derby embarcado.
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver").newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // tenta obter a conexão com o banco de dados derby embarcado,
+        // caso contrário imprime uma exceção.
+        try {
+            // Conecta no banco sem usuario e senha.
+            conn = DriverManager
+                    .getConnection("jdbc:derby:cc_console_db;create=true");
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        
-    }
-    
-    public static Connection getConnection() throws SQLException {
-        try {
-            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return DriverManager.getConnection("jdbc:derby:firstdb;create=true");
+
+        // Retorna a referência para a conexão com o banco.
+        return conn;
     }
 
 }
